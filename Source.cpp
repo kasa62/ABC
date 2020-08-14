1c469c9e44a32a15228d38400d41ffd7c81912bc
Mutsuhito K <paserisuta6210@gmail.com>
2020-08-14 22:19:30 +0900

    ABC148a

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+#include <map>
+#include <queue>
+#define rep(i,n) for (int i=0;i<(n);i++)
+
+using namespace std;
 
 int main() {
-	double pi = 3.141592;
-	int R;
-	scanf("%d", &R);
-
-	printf("%f", 2 * R * pi);
+	int a, b;
+	cin >> a >> b;
+	if (a + b == 3) {
+		cout << 3 << endl;
+	}
+	else if(a+b == 4){
+		cout << 2 << endl;
+	}
+	else {
+		cout << 1 << endl;
+	}
 	return 0;
 }
