ff5ec3a922c7ee0492de734df6e0b9c095776619
Mutsuhito K <paserisuta6210@gmail.com>
2020-08-15 19:41:49 +0900

    ABC148c

--- Source.cpp
+++ Source.cpp
@@ -12,17 +12,23 @@
 
 using namespace std;
 
+//ユークリッドの互除法
+//p,qの最大公約数を返す
+int gcd(int p, int q) {
+	if (p % q == 0) return q;
+	return gcd(q, p % q);
+}
+
+//最小公倍数を返す
+long long lcm(long long a, long long b) {
+	return a * b / gcd(a, b);
+}
+
 int main() {
-	int a, b;
+	long long a, b;
 	cin >> a >> b;
-	if (a + b == 3) {
-		cout << 3 << endl;
-	}
-	else if(a+b == 4){
-		cout << 2 << endl;
-	}
-	else {
-		cout << 1 << endl;
-	}
+
+	cout << lcm(a, b) << endl;
+
 	return 0;
 }
