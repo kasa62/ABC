cf5c7070a1b71dd3f99ac9c1865cd17f1ae7c9f6
Mutsuhito K <paserisuta6210@gmail.com>
2020-08-24 00:27:52 +0900

    ABC176D未完成

--- Source.cpp
+++ Source.cpp
@@ -11,77 +11,80 @@
 #define rep(i,n) for (int i=0;i<(n);i++)
 
 using namespace std;
+void warps(vector<vector<string>> s, vector<vector<int>> warp, int kaisuu, int h, int w, int x, int y);
+
+void walk(vector<vector<string>> s,vector<vector<int>> warp,int kaisuu,int h,int w,int x,int y) {
+	int flag = 0;
+	warp[x][y] = kaisuu;
+	if (x != 0) {
+		if (s[x - 1][y] == "." && warp[x - 1][y] != kaisuu) {
+			warp[x - 1][y] = kaisuu;
+			walk(s, warp, kaisuu, h, w, x - 1, y);
+		}
+		else if (s[x - 1][y] == "#") {
+			flag = 1;
+		}
+	
+	}
+	if (y != 0) {
+		if (s[x][y - 1] == "." && warp[x][y - 1] != kaisuu) {
+			warp[x][y - 1] = kaisuu;
+			walk(s, warp, kaisuu, h, w, x, y - 1);
+		}
+		else if (s[x][y - 1] == "#") {
+			flag = 1;
+		}
+	}
+	if (x != h - 1) {
+		if (s[x + 1][y] == "." && warp[x + 1][y] != kaisuu) {
+			warp[x + 1][y] = kaisuu;
+			walk(s, warp, kaisuu, h, w, x + 1, y);
+		}
+		else if (s[x + 1][y] == "#") {
+			flag = 1;
+		}
+	}
+	if (y != w - 1) {
+		if (s[x][y + 1] == "." && warp[x][y + 1] != kaisuu) {
+			warp[x][y + 1] = kaisuu;
+			walk(s, warp, kaisuu, h, w, x, y + 1);
+		}
+		else if (s[x][y + 1] == "#") {
+			flag = 1;
+		}
+	}
+
+}
+
+void warps(vector<vector<string>> s, vector<vector<int>> warp, int kaisuu, int h, int w, int x, int y) {
+	for (int i = x - 2;i <= x + 2;i++) {
+		if (i < 0 || i >= h)continue;
+		for (int j = y - 2;j <= y + 2;j++) {
+			if (j < 0 || j >= w)continue;
+			if (warp[i][j] == -1 && s[i][j] == ".") {
+				warp[i][j] = kaisuu;
+			}
+		}
+	}
+}
 
 int main() {
-	int n, k;
-	cin >> n >> k;
-	vector<int> p(n+1);
-	vector<int> c(n+1);
-	for (int i = 1;i <= n;i++) {
-		cin >> p[i];
-	}
-	for (int i = 1;i <= n;i++) {
-		cin >> c[i];
-	}
-
-	vector<int> junkan(n + 1);
-	long long max = -10000000000;
-	vector<long long> maxs(n + 1);
-
-	for (int i = 1;i < n;i++) {
-		maxs[i] = max;
-	}
-
-	int sikou, flag = 0;
-	int nokori, bai;
-	if (n > k) {
-		sikou = k;
-	}
-	else {
-		sikou = n;
-		flag = 1;
-	}
-
-	for(int i = 1;i <= n;i++) {
-		int next = p[i];
-		long long score = 0;
-		int flag_j = 0;
-		for (int j = 1;j <= sikou;j++) {
-			if (junkan[next] == i) {
-				flag_j = j - 1;
-				break;
-			}
-			else {
-				junkan[j] = i;
-			}
-			score += c[next];
-			next = p[next];
-			if (score > max) {
-				max = score;				
-			}
-		}
-
-		if (flag_j == 0) {
-			flag_j = sikou;
-		}
-
-		if (flag == 1) {
-			long long tmp = score * (k / flag_j);
-			int tmp_next = next;
-			if (tmp > max) {
-				max = tmp;
-			}
-			for (int a = 1;a <= k % flag_j;a++) {
-				tmp += c[tmp_next];
-				tmp_next = p[tmp_next];
-				if (tmp > max) {
-					max = tmp;
-				}
-			}
+	int h, w;
+	cin >> h >> w;
+	int ch, cw, dh, dw;
+	cin >> ch >> cw >> dh >> dw;
+	vector<vector<string>> s(h,vector<string> (w));
+	vector<vector<int>> warp(h, vector<int>(w));
+	for (int i = 0;i < h;i++) {
+		for (int j = 0;j < w;j++) {
+			cin >> s[i][j];
+			warp[i][j] = -1;
 		}
 	}
 
+	walk(s, warp, 0, h, w, ch, cw);
 
-	cout << max << endl;
+	cout << warp[dh][dw] << endl;
+
 	return 0;
 }
